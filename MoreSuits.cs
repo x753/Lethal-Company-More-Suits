bdb9666e4fadf578f5d1abb3904774f65de46763
753 <PrimaryInfinity@gmail.com>
2023-11-18 11:53:05 -0500

    Updated to v1.1.0
    Added support for emission and other advanced features.

--- MoreSuits.cs
+++ MoreSuits.cs
@@ -1,8 +1,8 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System;
 using System.IO;
 using System.Reflection;
-using Unity.Netcode;
 using UnityEngine;
 
 namespace MoreSuits
@@ -12,7 +12,7 @@ namespace MoreSuits
     {
         private const string modGUID = "x753.More_Suits";
         private const string modName = "More Suits";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "1.1.0";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -41,58 +41,133 @@ namespace MoreSuits
             [HarmonyPrefix]
             static void StartPatch(ref StartOfRound __instance)
             {
-                if (!SuitsAdded)
+                try
                 {
-                    for (int i = 0; i < __instance.unlockablesList.unlockables.Count; i++)
+                    if (!SuitsAdded)
                     {
-                        UnlockableItem unlockableItem = __instance.unlockablesList.unlockables[i];
-                        if (unlockableItem.suitMaterial != null && unlockableItem.alreadyUnlocked)
+                        for (int i = 0; i < __instance.unlockablesList.unlockables.Count; i++)
                         {
-                            // Get all .png files in the same folder as the mod
-                            string[] pngFiles = Directory.GetFiles(SuitsFolder, "*.png");
-
-                            // Create new suits for each .png
-                            foreach (string texturePath in pngFiles)
+                            UnlockableItem unlockableItem = __instance.unlockablesList.unlockables[i];
+                            if (unlockableItem.suitMaterial != null && unlockableItem.alreadyUnlocked)
                             {
-                                UnlockableItem newSuit;
-                                Material newMaterial;
+                                // Get all .png files in the same folder as the mod
+                                string[] pngFiles = Directory.GetFiles(SuitsFolder, "*.png");
 
-                                if (Path.GetFileNameWithoutExtension(texturePath) == "default")
+                                // Create new suits for each .png
+                                foreach (string texturePath in pngFiles)
                                 {
-                                    newSuit = unlockableItem;
-                                    newMaterial = newSuit.suitMaterial;
+                                    UnlockableItem newSuit;
+                                    Material newMaterial;
+
+                                    if (Path.GetFileNameWithoutExtension(texturePath) == "default")
+                                    {
+                                        newSuit = unlockableItem;
+                                        newMaterial = newSuit.suitMaterial;
+                                    }
+                                    else
+                                    {
+                                        // Serialize and deserialize to create a deep copy of the original suit item
+                                        string json = JsonUtility.ToJson(unlockableItem);
+                                        newSuit = JsonUtility.FromJson<UnlockableItem>(json);
+
+                                        newMaterial = Instantiate(newSuit.suitMaterial);
+                                    }
+
+                                    string filePath = Path.Combine(SuitsFolder, texturePath);
+                                    byte[] fileData = File.ReadAllBytes(filePath);
+                                    Texture2D texture = new Texture2D(2, 2);
+                                    texture.LoadImage(fileData);
+
+                                    newMaterial.mainTexture = texture;
+
+                                    // Optional modification of other properties like normal maps, emission, etc
+                                    // https://docs.unity3d.com/Packages/com.unity.render-pipelines.high-definition@14.0/manual/Lit-Shader.html
+                                    try
+                                    {
+                                        string advancedJsonPath = Path.Combine(SuitsFolder, "advanced", Path.GetFileNameWithoutExtension(texturePath) + ".json");
+                                        if (File.Exists(advancedJsonPath))
+                                        {
+                                            string[] lines = File.ReadAllLines(advancedJsonPath);
+
+                                            foreach (string line in lines)
+                                            {
+                                                string[] keyValue = line.Trim().Split(':');
+                                                if (keyValue.Length == 2)
+                                                {
+                                                    string keyData = keyValue[0].Trim('"', ' ', ',');
+                                                    string valueData = keyValue[1].Trim('"', ' ', ',');
+
+                                                    if (float.TryParse(valueData, out float floatValue))
+                                                    {
+                                                        newMaterial.SetFloat(keyData, floatValue);
+                                                    }
+                                                    else if (valueData == "KEYWORD")
+                                                    {
+                                                        newMaterial.EnableKeyword(keyData);
+                                                    }
+                                                    else if (valueData.Contains(".png"))
+                                                    {
+                                                        string advancedTexturePath = Path.Combine(SuitsFolder, "advanced", valueData);
+                                                        byte[] advancedTextureData = File.ReadAllBytes(advancedTexturePath);
+                                                        Texture2D advancedTexture = new Texture2D(2, 2);
+                                                        advancedTexture.LoadImage(advancedTextureData);
+
+                                                        newMaterial.SetTexture(keyData, advancedTexture);
+                                                    }
+                                                    else if (TryParseVector4(valueData, out Vector4 vectorValue))
+                                                    {
+                                                        newMaterial.SetVector(keyData, vectorValue);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Debug.Log("Something went wrong with More Suits! Error: " + ex);
+                                    }
+
+                                    newSuit.suitMaterial = newMaterial;
+                                    newSuit.unlockableName = Path.GetFileNameWithoutExtension(filePath);
+
+                                    if (Path.GetFileNameWithoutExtension(texturePath) != "default")
+                                    {
+                                        __instance.unlockablesList.unlockables.Add(newSuit);
+                                    }
                                 }
-                                else
-                                {
-                                    // Serialize and deserialize to create a deep copy of the original suit item
-                                    string json = JsonUtility.ToJson(unlockableItem);
-                                    newSuit = JsonUtility.FromJson<UnlockableItem>(json);
 
-                                    newMaterial = Instantiate(newSuit.suitMaterial);
-                                }
-
-                                string filePath = Path.Combine(SuitsFolder, texturePath);
-                                byte[] fileData = File.ReadAllBytes(filePath);
-                                Texture2D texture = new Texture2D(2, 2);
-                                texture.LoadImage(fileData);
-
-                                newMaterial.mainTexture = texture;
-
-                                newSuit.suitMaterial = newMaterial;
-                                newSuit.unlockableName = Path.GetFileNameWithoutExtension(filePath);
-
-                                if (Path.GetFileNameWithoutExtension(texturePath) != "default")
-                                {
-                                    __instance.unlockablesList.unlockables.Add(newSuit);
-                                }
+                                SuitsAdded = true;
+                                return;
                             }
-
-                            SuitsAdded = true;
-                            return;
                         }
                     }
                 }
+                catch (Exception ex)
+                {
+                    Debug.Log("Something went wrong with More Suits! Error: " + ex);
+                }
             }
+        }
+
+        public static bool TryParseVector4(string input, out Vector4 vector)
+        {
+            vector = Vector4.zero;
+
+            string[] components = input.Split(',');
+
+            if (components.Length == 4)
+            {
+                if (float.TryParse(components[0], out float x) &&
+                    float.TryParse(components[1], out float y) &&
+                    float.TryParse(components[2], out float z) &&
+                    float.TryParse(components[3], out float w))
+                {
+                    vector = new Vector4(x, y, z, w);
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-# More Suits v1.0.0
-### Adds more default suits to choose from, and allows you to customize them!
+# More Suits v1.1.0
+### Adds more suits to choose from, and allows you to customize them!
 
 ## Instructions
-Place the ```x753-More_Suits-X.X.X``` folder in your ```BepInEx/Plugins``` folder.
+Place the ```x753-More_Suits-X.X.X``` folder in your ```BepInEx/Plugins``` folder. Make sure the ```suits``` folder is in the same folder as ```MoreSuits.dll```.
 
 ## Customize
 You can add .png files to the ```suits``` folder to add new suits as long as both the host and clients have the same files.
 
+## Advanced
+You can add a .json file in the ```advanced``` folder with the same name as your .png file in the ```suits``` folder to enable additional features like emission. Place additional texture maps in the ```advanced``` folder.
+
+For a list of supported features, see:
+https://docs.unity3d.com/Packages/com.unity.render-pipelines.high-definition@14.0/manual/Lit-Shader.html
+
 ## Changelog
+	- v1.1.0
+		- Added support for advanced features (normal maps, emission, etc)
 	- v1.0.0
 		- Release
